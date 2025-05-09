243e6e8e284d35f001810fc6f69cf7ba063e8fca
Cabrel10 <87647644+Cabrel10@users.noreply.github.com>
2025-05-09 08:00:41 +0100

    Update setup_env_vars.py
    api solve

--- scripts/setup_env_vars.py
+++ scripts/setup_env_vars.py
@@ -10,10 +10,10 @@ import os
 import sys
 
 # Définir les clés API
-os.environ['GOOGLE_API_KEY_1'] = 'AIzaSyBxT1xuZf_3CsnEnQs__HZJX-S_Ak6NS0E'
-os.environ['GOOGLE_API_KEY_2'] = 'AIzaSyAnWLOQ1NemZEi1YMOIk2NIjVyOgJ66XfQ'
-os.environ['COINMARKETCAP_API_KEY'] = '2b7f5480-8df2-4c84-a22a-8d9f66d4d580'
-os.environ['COINMARKETCAP_API_KEY_2'] = '0853fffa-27e7-45c7-b549-5e857416275f'
+os.environ['GOOGLE_API_KEY_1'] = 'api'
+os.environ['GOOGLE_API_KEY_2'] = 'api'
+os.environ['COINMARKETCAP_API_KEY'] = 'api'
+os.environ['COINMARKETCAP_API_KEY_2'] = 'api'
 
 # Définir GEMINI_API_KEY comme étant égal à GOOGLE_API_KEY_1 pour la compatibilité
 os.environ['GEMINI_API_KEY'] = os.environ['GOOGLE_API_KEY_1']
